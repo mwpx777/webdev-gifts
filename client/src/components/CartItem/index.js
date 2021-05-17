90e7d06d06ad258b7f9f5200fe98cd71a437d3b4
MWPX777 <mwpx777@gmail.com>
2021-05-17 08:31:06 -0500

    categoryMenu completed

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { useStoreContext } from '../../utils/GlobalState';
-import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions',
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 import { idbPromise } from '../../utils/helpers';
 
 const CartItem = ({ item }) => {
---
+++ client/src/components/CategoryMenu/index.js
@@ -0,0 +1,55 @@
+// CATEGORY MENU
+
+import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_CATEGORIES } from '../../utils/queries';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
+import { useStoreContext } from '../../utils/GlobalState';
+import { idbPromise } from '../../utils/helpers';
+
+function CategoryMenu({ setCategory }) {
+    const [state, dispatch] = useStoreContext();
+    const { categories } = state;
+    const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+    useEffect(() => {
+        if (CategoryData) {
+            dispatch({
+                type: UPDATE_CATEGORIES,
+                categories: categoryData.categories
+            });
+            categoryData.categories.forEach(category => {
+                idbPromise('categories', 'put', category);
+            });
+        } else if (!loading) {
+            idbPromise('categories', 'get').then(categories => {
+                dispatch({
+                    type: UPDATE_CATEGORIES,
+                    categories: categories
+                });
+            });
+        }
+    }, [loading, categoryData, dispatch]);
+
+    const handleClick = id => {
+        dispatch({
+            type: UPDATE_CURRENT_CATEGORY,
+            currentCategory: id
+        });
+    };
+
+    return (
+        <div>
+            <h2> Categories</h2>
+            {categories.map(item => (
+                <button
+                    key={item._id}
+                    onClick={() => {
+                        handleClick(item._id);
+                    }}> {item.name}</button>
+            ))}
+        </div>
+    );
+};
+
+export default CategoryMenu;
