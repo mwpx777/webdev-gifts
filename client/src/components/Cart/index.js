95063ea9c542ff3fa3dfc761504d1f7bf6c7480e
MWPX777 <mwpx777@gmail.com>
2021-05-17 07:51:27 -0500

    finished with cart component

---
+++ client/src/components/Cart/index.js
@@ -0,0 +1,110 @@
+// CART
+
+import React from 'react';
+import CartItem from '../CartItem';
+import Auth from '../../utils/auth';
+import './styles.css';
+import { useLazyQuery } from '@apollo/react-hooks';
+import { useStoreContext } from '../../utils/GlobalState';
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+import { idbPromise } from '../../utils/helpers';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
+const Cart = () => {
+    const [state, dispatch] = useStoreContext();
+
+    // only run when checkout is called
+    // data contains checkout info
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
+    useEffect(() => {
+        if (data) {
+            stripePromise.then((res) => {
+                res.redirectToCheckout({ sessionId: data.checkout.session })
+            })
+        }
+    }, [data])
+
+    useEffect(() => {
+        async function getCart() {
+            const cart = await idbPromise('cart', 'get');
+            dispatch({
+                type: ADD_MULTIPLE_TO_CART,
+                products: [...cart]
+            });
+        };
+        // if nothing in state for cart, run getCart function to get items from objectStore and save to GlobalState object
+        if (!state.cart.length) {
+            getCart();
+        }
+    }, [state.cart.length, dispatch]);
+
+    function toggleCart() {
+        dispatch({
+            type: TOGGLE_CART
+        });
+    }
+    if (!state.cartOpen) {
+        return (
+            <div className="cart-closed" onClick={toggleCart}>
+                <span role="img" aria-label="trash">🛒</span>
+            </div>
+        );
+    };
+
+    function calculateTotal() {
+        let sum = 0;
+        state.cart.forEach(item => {
+            sum += item.price * item.purchaseQuantity;
+        });
+        return sum.toFixed(2);
+    }
+
+    function submitCheckout() {
+        const productIds = [];
+        // loop over items in cart and add their item._id to productIds empty array
+        state.cart.forEach(item => {
+            for (let i = 0; i < item.purchaseQuantity; i++) {
+                productIds.push(item._id);
+            }
+        });
+        getCheckout({
+            // the productIds get passed into {data} for const getCheckout method above
+            variables: { products: productIds }
+        })
+    }
+
+    return (
+        <div className="cart">
+            <div className="close" onClick={toggleCart}>Close</div>
+            <h2>Shopping Cart</h2>
+            {state.cart.length ? (
+                <div>
+                    {state.cart.map(item => {
+                        <CartItem key={item._id} item={item} />
+                    })}
+                    <div className="flex-row space-between">
+                        <strong>Total: ${calculateTotal()}</strong>
+                        {
+                            Auth.loggedIn() ?
+                                <button onClick={submitCheckout}>Checkout</button>
+                                :
+                                <span>Login to checkout!</span>
+                        }
+                    </div>
+                </div>
+            ) : (
+                <h3>
+                    <span role="img" aria-label="shocked">😱</span>
+                    You haven't added any items to your cart yet!
+                </h3>
+            )}
+        </div>
+
+    )
+};
+
+export default Cart;
+
