949e4dc2e3781df0d740fa51cb00af0f67760170
AdamR-Work <aramosatwork@hotmail.com>
2021-05-20 22:15:29 -0500

    troubleshooting this nav bar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,7 +7,7 @@ import icon from '../../assets/WebDev logo.png'
 import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_CATEGORIES } from '../../utils/queries';
-import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
+import { TOGGLE_NAV, UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { useStoreContext } from '../../utils/GlobalState';
 import { idbPromise } from '../../utils/helpers';
 
@@ -24,9 +24,13 @@ function Nav({ setCategory }) {
         if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 75) {
             document.getElementById("navBar").setAttribute("class", "endState")
             document.getElementById("navBarItems").setAttribute("class", "displaynone")
+            document.getElementById("navTitle").setAttribute("class", "displayhidden")
+            document.getElementById("navGreeting").setAttribute("class", "displayhidden")
         }else {
             document.getElementById("navBar").setAttribute("class", "initialState")
             document.getElementById("navBarItems").setAttribute("class", "flex-row-right")
+            document.getElementById("navTitle").setAttribute("class", "flex-row-right")
+            document.getElementById("navGreeting").setAttribute("class", "greeting")
         }
     }
     useEffect(() => {
@@ -55,13 +59,60 @@ function Nav({ setCategory }) {
         });
     };
 
-    // function openNav() {
-    //     document.getElementById("mySidenav").style.width = "250px";
-    //   }
+
+
+let isNavOpen=()=> {
+
+        var test = document.getElementById("navBar");
+if(test.classList.contains("endState")){
+    console.log("open")
+}
+if(test.classList.contains("initialState")){
+    console.log("closed")
+}
+
+
+
+// switch (test) {
+//   case test.classList.contains("initialState"):
+//     test.innerHTML = "I have class1";
+//     console.log("IS")
+//     break;
+//   case test.classList.contains("endState"):
+//     test.innerHTML = "I have class2";
+//     console.log("ES")
+//     break;
+//   default:
+//     test.innerHTML = "";
+// }
+
+    } 
+
+    // function toggleNav(){
+    //     dispatch({type:TOGGLE_NAV});
+    // }
+    //  if(!state.openNav){
+    //     // document.getElementById("navBar").setAttribute("onClick", "")
+    //     document.getElementById("navBar").setAttribute("class", "endState")
+    // }else{
+        
+    //     document.getElementById("navBar").setAttribute("class", "initialState")
+    //     document.getElementById("navBar").setAttribute("onClick", "{toggleNav}")
+
+    // }
+
+    function openNav() {
+        // document.getElementById("mySidenav").style.width = "250px";
+        if(openNav)
+        console.log("open")
+        if(!openNav){
+            console.log("closed")
+        }
+      }
       
-    //   function closeNav() {
-    //     document.getElementById("mySidenav").style.width = "0";
-    //   }
+      function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+      }
 
 
     function showNavigation() {
@@ -71,7 +122,7 @@ function Nav({ setCategory }) {
                      {categories.map(item => (
                 <button className="navButton" key={item._id}onClick={() => {handleClick(item._id);}}> {item.name}</button>))}
                     <li className="mx-1">
-                        <Link to="/orderHistory">
+                        <Link className="navButton" to="/orderHistory">
                         <button className="navButton">   Order History</button>
                         </Link>
                     </li>
@@ -94,12 +145,12 @@ function Nav({ setCategory }) {
                     }}> {item.name}</button>
             ))}
                     <button className="mx-1 navButton">
-                        <Link to="/signup">
+                        <Link className="navButton"to="/signup">
                             Signup
                         </Link>
                     </button>
                     <button className="mx-1 navButton">
-                        <Link to="/login">
+                        <Link className="navButton" to="/login">
                             Login
                         </Link>
                     </button>
@@ -108,20 +159,20 @@ function Nav({ setCategory }) {
             );
         }
     }
-
+isNavOpen()
     return (
-        <header className="navHeader">
-            <h1 className="flex-row-right">
+        <header id="navHeader" className="navHeader">
+            <h1 id="navTitle"className="flex-row-right">
                 <Link to="/" className="flex-row-right">
-                    <img src={icon} className="roundicon" id="icon"></img>
+                    <img src={icon} className="roundicon" alt="web dev logo"id="icon"></img>
                     <span className="title">2020-2021 WebDev Gifts</span>
                 </Link>
                 </h1>
-            <div id="navBar" className="navBarChoices">
+            <div id="navBar" onClick={()=>{openNav()}} className="navBarChoices">
                 {showNavigation()}
             </div>
 
-            <p className="greeting">Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p id="navGreeting" className="greeting">Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </header>
     );
 }
