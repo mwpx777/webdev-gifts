e66f6f39bdfab4060d9222bbcfb6fc0ba11a835d
AdamR-Work <aramosatwork@hotmail.com>
2021-05-22 14:01:22 -0500

    navBar Works how I wanted finally. now just working on styling

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -19,22 +19,22 @@ function Nav({ setCategory }) {
     const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
     
    
-    // window.onscroll = function(){
-    //     scrollFn()
-    // }
-    // function scrollFn() {
-    //     if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 75) {
-    //         document.getElementById("navBar").setAttribute("class", "endState")
-    //         document.getElementById("navBarItems").setAttribute("class", "displaynone")
-    //         document.getElementById("navTitle").setAttribute("class", "displayhidden")
-    //         document.getElementById("navGreeting").setAttribute("class", "displayhidden")
-    //     }else {
-    //         document.getElementById("navBar").setAttribute("class", "initialState")
-    //         document.getElementById("navBarItems").setAttribute("class", "flex-row-right")
-    //         document.getElementById("navTitle").setAttribute("class", "flex-row-right")
-    //         document.getElementById("navGreeting").setAttribute("class", "greeting")
-    //     }
-    // }
+    window.onscroll = function(){
+        scrollFn()
+    }
+    function scrollFn() {
+        if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 75) {
+            document.getElementById("navBar").setAttribute("class", "endState")
+            document.getElementById("navBarItems").setAttribute("class", "displaynone")
+            document.getElementById("navTitle").setAttribute("class", "displayhidden")
+            document.getElementById("navGreeting").setAttribute("class", "displayhidden")
+        }else {
+            document.getElementById("navBar").setAttribute("class", "initialState")
+            document.getElementById("navBarItems").setAttribute("class", "flex-row-right")
+            document.getElementById("navTitle").setAttribute("class", "flex-row-right")
+            // document.getElementById("navGreeting").setAttribute("class", "greeting")
+        }
+    }
     useEffect(() => {
         if (categoryData) {
             dispatch({
@@ -69,77 +69,35 @@ function Nav({ setCategory }) {
 
 
 function isNavOpen(){
+ 
+    var test = document.getElementById("navBar")
 
-        var test = document.getElementById("navBar");
 if(test.classList.contains("endState")){
+    console.log("closed")
+    document.getElementById("navBar").setAttribute("class", "initialState2")
+    document.getElementById("navBarItems").setAttribute("class", "flex-row-right2")
+
+}
+else{
     console.log("open")
 }
-if(test.classList.contains("initialState")){
-    console.log("closed")
 }
-}
-
-
-// switch (test) {
-//   case test.classList.contains("initialState"):
-//     test.innerHTML = "I have class1";
-//     console.log("IS")
-//     break;
-//   case test.classList.contains("endState"):
-//     test.innerHTML = "I have class2";
-//     console.log("ES")
-//     break;
-//   default:
-//     test.innerHTML = "";
-// }
-
-    // } 
-
-    // function toggleNav(){
-    //     dispatch({type:TOGGLE_NAV});
-    // }
-    //  if(!state.openNav){
-    //     // document.getElementById("navBar").setAttribute("onClick", "")
-    //     document.getElementById("navBar").setAttribute("class", "endState")
-    // }else{
-        
-    //     document.getElementById("navBar").setAttribute("class", "initialState")
-    //     document.getElementById("navBar").setAttribute("onClick", "{toggleNav}")
-
-    // }
-
-    function openNav() {
-        // document.getElementById("mySidenav").style.width = "250px";
-        if(openNav)
-        console.log("open")
-        if(!openNav){
-            console.log("closed")
-        }
-      }
-      
-      function closeNav() {
-        document.getElementById("mySidenav").style.width = "0";
-      }
 
 
     function showNavigation() {
         if (Auth.loggedIn()) {
             return (
-                <ul id="navBarItems" className=" flex-row-right">
+                <div id="navBarItems" className=" flex-row-right">
                      {categories.map(item => (
                 <button className="navButton" key={item._id}onClick={() => {handleClick(item._id);}}> {item.name}</button>))}
-                    <li className="mx-1">
+                   
                         <Link className="navButton" to="/orderHistory">
                         <button className="navButton">   Order History</button>
                         </Link>
-                    </li>
-                    <li className="mx-1">
                         <button className="navButton" href="/" onClick={() => Auth.logout()}>
                             Logout
                         </button>
-                    </li>
-                   
-                </ul>
+                </div>
             );
         } else {
             return (
@@ -177,11 +135,16 @@ if(test.classList.contains("initialState")){
                     <span className="title">2020-2021 WebDev Gifts</span>
                 </Link>
                 </h1>
-            <div id="navBar" onClick={()=>{openNav()}} className="navBarChoices">
+            <div id="navBar" onClick={()=>{isNavOpen()}} className="navBarChoices">
                 {showNavigation()}
             </div>
+        <div id="holder">
+            <div class="firework" id="firework2"></div>
 
-            <p id="navGreeting" className="greeting">Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p id="navGreeting" className="greeting">Congrats you did IT! It was a crazy 6 Months but you are a full stack Developer.
+            <br></br> Now get yourself some new gear to sport your accomplishments.</p>
+            <div class="firework" id="firework1"></div>
+        </div>
         </header>
     );
 }
